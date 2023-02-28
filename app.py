0b4e724f4d1a3d6b5d24ecde94ef2326fcd0d5a2
Hannes <hannes.porombka@gmail.com>
2023-02-28 19:29:44 +0100

    Highscore and Game count working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from boggle import Boggle
 
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, redirect, jsonify, request, render_template, session, redirect
 
 app = Flask(__name__)
 
@@ -9,6 +9,7 @@ app.config["SECRET_KEY"] = "sehrgeheim"
 
 boggle_game = Boggle()
 game_board = []
+
 
 
 
@@ -23,8 +24,18 @@ def start_game():
     game_board = boggle_game.make_board()
     session['board'] = game_board
     session['words'] = []
-    return render_template('game.html', board=game_board)
+    session ['num_of_game'] = 0
+    session ['high_score'] = 0
+    return render_template('game.html', board=game_board, num_games = session ['num_of_game'], high_score = session ['high_score'])
 
+@app.route('/newgame')
+def new_game():
+    game_board = boggle_game.make_board()
+    session['board'] = game_board
+    high_score = session ['high_score']
+    num_of_game = session['num_of_game']
+    print("new game")
+    return render_template('game.html', board=game_board, num_games = session ['num_of_game'], high_score = session ['high_score'])
 
 
 @app.route('/guess', methods= ['POST'])
@@ -39,3 +50,14 @@ def validate_guess():
     return boggle_game.check_valid_word(session['board'], guess)
     
 
+@app.route('/gameend', methods= ['POST'])
+def end_game():
+    score =  request.get_json()['score']
+    session["num_of_game"] += 1
+    if score > session["high_score"]:
+        session["high_score"] = score
+    print (session["high_score"])
+    print (session["num_of_game"])
+    return redirect('/newgame')
+    
+    
--- static/app.js
+++ static/app.js
@@ -4,16 +4,22 @@ const $subButton = $('#submitguess')
 const $guessInput = $('#guess')
 const $score = $('#score')
 const $time = $('#timer')
+const $restartButton = $('#restart')
+const $all = $("*")
+
+let score = 0;
+
 
 let count = ''
 
-function countdown(time) {
+async function game_countdown(time) {
     let timer = setInterval(function () {
         time--;
         if (time <= 0) {
             clearInterval(timer);
             count = 0
             console.log(count)
+            
         }
         else {
             count = time
@@ -24,9 +30,25 @@ function countdown(time) {
     }, 1000)
 }
 
-countdown(60)
+game_countdown(60)
 
-let score = 0;
+async function send_score(points){
+    const response = await axios({
+        url: `/gameend`,
+        method: "POST",
+        data: { score: points },
+
+    })
+
+    return response.data
+}
+
+$restartButton.on("click", async function(){
+    console.log("Button works")
+    await send_score(score)
+    window.location.replace('/newgame')
+})
+
 
 $subButton.on("click", async function (e) {
     e.preventDefault()
