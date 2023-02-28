768fcca57b6b27c4f080376c905ba5ee33aa0874
Hannes <hannes.porombka@gmail.com>
2023-02-28 13:18:32 +0100

    added timer and updated score to count word length

--- static/app.js
+++ static/app.js
@@ -3,19 +3,42 @@
 const $subButton = $('#submitguess')
 const $guessInput = $('#guess')
 const $score = $('#score')
+const $time = $('#timer')
 
+let count = ''
+
+function countdown(time) {
+    let timer = setInterval(function () {
+        time--;
+        if (time <= 0) {
+            clearInterval(timer);
+            count = 0
+            console.log(count)
+        }
+        else {
+            count = time
+            console.log(count)
+        }
+        $time.text(`Remaining Time: ${count}`)
+
+    }, 1000)
+}
+
+countdown(60)
 
 let score = 0;
 
 $subButton.on("click", async function (e) {
     e.preventDefault()
-    const result = await sendGuess($guessInput.val())
-    if (result.data === "ok"){
-        score += 1
-        $score.text(`Score: ${score}`)
-        
+    if (count != 0) {
+        const result = await sendGuess($guessInput.val())
+        if (result.data === "ok") {
+            score += $guessInput.val().length
+            $score.text(`Score: ${score}`)
+
+        }
+        $('#result').text(`Last guess: ${$guessInput.val().toUpperCase()}     ---->     ${result.data.toUpperCase()}`)
     }
-    $('#result').text(`Last guess: ${$guessInput.val().toUpperCase()}     ---->     ${result.data.toUpperCase()}`)
 })
 
 
